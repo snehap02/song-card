5bb54670b9551fb61f1f24d6d72d32c37ff89f88
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-02-09 14:03:44 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        montserrat: 'Montserrat',
+        poppins: 'Poppins',
+        raleway: 'Raleway'
+      }
+    },
+  },
+  plugins: [],
+}
